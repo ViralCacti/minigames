c63b0ddc934a1f43ea316b3c0a349bbfdd51643a
ViralCacti <87978621+ViralCacti@users.noreply.github.com>
2021-08-10 07:18:28 +0200

    50/50

---
+++ 50 50.py
@@ -0,0 +1,13 @@
+# 50/50 project developed by ViralCactus / Ian RQJ / Naibush / Quiño :-)
+# Run the project
+
+import random
+import math
+
+print("50/50 ~ ViralCactus")
+svar1 = input("[50/50] Mata in första halvan: \n>>")
+svar2 = input("\n[50/50] Mata in andra halvan: \n>>")
+
+samladesvar = [svar1, svar2]
+
+print("\n[50/50]: " + random.choice(samladesvar))
